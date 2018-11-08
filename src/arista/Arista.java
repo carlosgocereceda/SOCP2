9f0a53df65652d484929b28e488d1bb3acb9fd2e
carlosgocereceda <carlosgcereceda@hotmail.es>
2018-11-08 15:08:22 +0100

    clase arista creada

---
+++ src/arista/Arista.java
@@ -0,0 +1,30 @@
+package arista;
+
+public class Arista {
+
+	private int nodo1;
+	private int nodo2;
+	
+	public Arista(int nodo1, int nodo2) {
+		this.nodo1 = nodo1;
+		this.nodo2 = nodo2;
+	}
+
+	public int getNodo1() {
+		return nodo1;
+	}
+
+	public void setNodo1(int nodo1) {
+		this.nodo1 = nodo1;
+	}
+
+	public int getNodo2() {
+		return nodo2;
+	}
+
+	public void setNodo2(int nodo2) {
+		this.nodo2 = nodo2;
+	}
+	
+	
+}
