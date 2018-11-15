6188e3a021f049c8dad22df0d6ec2f6b20f8785d
Adrian Sanchez <adsanc03@ucm.es>
2018-11-15 15:01:45 +0100

    Red y Arista agregados

--- src/modeloBarabasiAlbert/Arista.java
+++ src/modeloBarabasiAlbert/Arista.java
@@ -2,27 +2,27 @@ package modeloBarabasiAlbert;
 
 public class Arista {
 
-	private int nodo1;
-	private int nodo2;
+	private Nodo nodo1;
+	private Nodo nodo2;
 
-	public Arista(int nodo1, int nodo2) {
+	public Arista(Nodo nodo1, Nodo nodo2) {
 		this.nodo1 = nodo1;
 		this.nodo2 = nodo2;
 	}
 
-	public int getNodo1() {
+	public Nodo getNodo1() {
 		return nodo1;
 	}
 
-	public void setNodo1(int nodo1) {
+	public void setNodo1(Nodo nodo1) {
 		this.nodo1 = nodo1;
 	}
 
-	public int getNodo2() {
+	public Nodo getNodo2() {
 		return nodo2;
 	}
 
-	public void setNodo2(int nodo2) {
+	public void setNodo2(Nodo nodo2) {
 		this.nodo2 = nodo2;
 	}
 
@@ -30,12 +30,12 @@ public class Arista {
 	public boolean equals(Object o) {
 		return o != null &&
 				o instanceof Arista &&
-				( ((Arista) o).nodo1 == this.nodo1 && ((Arista) o).nodo2 == this.nodo2 ||
-				((Arista) o).nodo1 == this.nodo2 && ((Arista) o).nodo2 == this.nodo1);
+				( ((Arista) o).nodo1.equals(this.nodo1) && ((Arista) o).nodo2.equals(this.nodo2) ||
+				((Arista) o).nodo1.equals(this.nodo2) && ((Arista) o).nodo2.equals(this.nodo1));
 	}
 	
 	@Override
 	public String toString() {
-		return this.nodo1 + " " + this.nodo2;
+		return this.nodo1.toString() + " " + this.nodo2.toString();
 	}
 }
---
+++ src/modeloBarabasiAlbert/Red.java
@@ -0,0 +1,9 @@
+package modeloBarabasiAlbert;
+
+import java.util.List;
+
+public class Red {
+	List<Arista> aristas;
+	
+	
+}
