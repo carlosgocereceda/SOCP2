50095afa9b3a1923bc3785c05acd83d08d987863
Adrian Sanchez <adsanc03@ucm.es>
2018-11-15 15:05:58 +0100

    Constructora GeneraBarabasiAlbert

--- src/modeloBarabasiAlbert/GenerarBarabasiAlbert.java
+++ src/modeloBarabasiAlbert/GenerarBarabasiAlbert.java
@@ -1,5 +1,17 @@
 package modeloBarabasiAlbert;
 
 public class GenerarBarabasiAlbert {
+	private int M; // Num. de enlaces con los que entra un nodo nuevo a la red
+	private int T; // numero de pasos del modelo
+	private int m0; // Nodos iniciales (todos conectados con todos)
+	private int N; // Numero de nodos
+	
+	public GenerarBarabasiAlbert (int m, int t) {
+		this.M = m;
+		this.T = t;
+		this.m0 = m + 1;
+		this.N = this.m0 + t;
+	}
+	
 	
 }
