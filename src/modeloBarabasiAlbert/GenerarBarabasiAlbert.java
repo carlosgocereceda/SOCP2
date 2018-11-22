57edfb122462bb766da792d27c62210b859b1f78
Adrian Sanchez <adsanc03@ucm.es>
2018-11-22 15:54:14 +0100

    Pendiente terminar bucle

--- src/modeloBarabasiAlbert/GenerarBarabasiAlbert.java
+++ src/modeloBarabasiAlbert/GenerarBarabasiAlbert.java
@@ -24,35 +24,38 @@ public class GenerarBarabasiAlbert {
 				}
 			}
 		}
-		System.out.println("Red inicial creada con " + red.numNodos() + " nodos y " + red.numAristas() + " aristas.");
-
+		System.out.println("Red inicial creada con " + red.numNodos() + " nodos y " + red.numAristas() + " aristas."
+				+ System.getProperty("line.separator") + red);
 		return red;
 	}
 
 	public Red simularRed() {
 		Red red = this.generaRedInicial();
-		Ruleta ruleta = new Ruleta(red);
 		
-		for (int i = 0; i < this.T; i++) {
+		for (int i = red.numNodos() + 1; i < this.T + red.numNodos() + 1; i++) {
 			Nodo n1 = red.getExistingNodo(i); // Nuevo nodo
-			
-			while(n1.getDegree() <= this.M) { // Genera m <= m0 enlaces
+
+			while (n1.getDegree() <= this.M) { // Genera m <= m0 enlaces
+				Ruleta ruleta = new Ruleta(red);
+				
 				// Método de selección por ruleta
 				Integer in2 = ruleta.barabasi();
 				Nodo n2 = in2 != null ? red.getNodo(in2.intValue()) : null;
-				
-				if(in2 == null || n2 == null) {
+
+				if (in2 == null || n2 == null) {
 					System.err.println("ERROR: Ruleta ha generado un nodo: " + in2 + " no existente en la red.");
 					return red; // Salir del error
 				} else {
 					Arista a = new Arista(n1, n2);
-					if(!n1.equals(n2) && !red.contains(a)) {
+					if (!n1.equals(n2) && !red.contains(a)) {
 						red.add(a);
+					} else {
+						System.out.println("Ya contenia la arista: " + a);
 					}
 				}
 			}
 		}
-		
+
 		return red;
 	}
 
